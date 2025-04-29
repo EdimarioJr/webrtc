81bbf5b41a5475d37144008c8cde2e1e58c36b2c
EdimarioJr <edimariojunior.14@gmail.com>
2025-04-29 16:22:24 -0300

    signals

---
+++ server/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("join-room", (roomId: string) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("peer-joined", socket.id);
+  });
+
+  socket.on("offer", ({ roomId, offer }) => {
+    socket.to(roomId).emit("offer", { offer, from: socket.id });
+  });
+
+  socket.on("answer", ({ roomId, answer }) => {
+    socket.to(roomId).emit("answer", { answer, from: socket.id });
+  });
+
+  socket.on("ice-candidate", ({ roomId, candidate }) => {
+    socket.to(roomId).emit("ice-candidate", { candidate, from: socket.id });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+const PORT = 3002;
+httpServer.listen(PORT, () => {
+  console.log(`Signaling server running on port ${PORT}`);
+});
